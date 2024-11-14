ed42c8a3b875f91e37b52a168c5ce5ba26809a31
Memfis <Memfis-745@yandex.ru>
2024-11-15 01:01:25 +0300

    тз-15-1

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,10 +1,5 @@
 package ru.practicum.shareit.request.dto;
 
-import lombok.*;
-import ru.practicum.shareit.item.dto.ItemDto;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 /**
  * TODO Sprint add-item-requests.
